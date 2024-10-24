3f2b5bbaff50633f4c27e410fb6bf5350522330c
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 12:17:40 -0300

    feat: CandidateService creation

---
+++ src/main/java/com/gcd/vacancy/service/CandidateService.java
@@ -0,0 +1,10 @@
+package com.gcd.vacancy.service;
+
+import com.gcd.vacancy.dto.CandidatePostDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CandidateService {
+    void createCandidate(CandidatePostDto candidatePostDto);
+
+}
