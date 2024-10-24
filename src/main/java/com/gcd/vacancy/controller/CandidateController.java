1eafdabdebf85b7e43998289e02051defdeed2ab
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 12:17:04 -0300

    feat: CandidateController creation

---
+++ src/main/java/com/gcd/vacancy/controller/CandidateController.java
@@ -0,0 +1,26 @@
+package com.gcd.vacancy.controller;
+
+import com.gcd.vacancy.dto.CandidatePostDto;
+import com.gcd.vacancy.service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @Autowired
+    private CandidateService candidateService;
+
+    @PostMapping
+    public ResponseEntity createCandidate(@RequestBody CandidatePostDto candidatePostDto) {
+        candidateService.createCandidate(candidatePostDto);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
