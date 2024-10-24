cf1f61f3ac1d19877000efb5dd7e331a5f3605db
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 12:17:47 -0300

    feat: CandidateServiceImpl creation

---
+++ src/main/java/com/gcd/vacancy/service/CandidateServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gcd.vacancy.service;
+
+import com.gcd.vacancy.dto.CandidatePostDto;
+import com.gcd.vacancy.entity.CandidateEntity;
+import com.gcd.vacancy.mapper.CandidateMapper;
+import com.gcd.vacancy.repository.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CandidateServiceImpl implements CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private CandidateMapper candidateMapper;
+
+    @Override
+    public void createCandidate(CandidatePostDto candidatePostDto) {
+        CandidateEntity newCandidate = candidateMapper.toCandidateEntity(candidatePostDto);
+
+        candidateRepository.save(newCandidate);
+    }
+}
