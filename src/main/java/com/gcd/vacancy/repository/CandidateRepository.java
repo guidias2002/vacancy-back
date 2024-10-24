b59a17d534620084e2e59274a2d666fe018906f4
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 12:17:32 -0300

    feat: CandidateRepository creation

---
+++ src/main/java/com/gcd/vacancy/repository/CandidateRepository.java
@@ -0,0 +1,9 @@
+package com.gcd.vacancy.repository;
+
+import com.gcd.vacancy.entity.CandidateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
+}
