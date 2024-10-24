333a2fa12181e2fbb4f8e49614be4f313dfb9eeb
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 12:15:32 -0300

    feat: first commit

---
+++ src/test/java/com/gcd/vacancy/VacancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcd.vacancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
