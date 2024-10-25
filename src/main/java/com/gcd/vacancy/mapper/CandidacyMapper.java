12d5849a4e044f46a997ef700a89c99a4a8e88ab
Guilherme Dias <guidias200220@gmail.com>
2024-10-25 11:53:10 -0300

    feat: CandidacyMapper created

---
+++ src/main/java/com/gcd/vacancy/mapper/CandidacyMapper.java
@@ -0,0 +1,12 @@
+package com.gcd.vacancy.mapper;
+
+import com.gcd.vacancy.dto.ApplicationSentDto;
+import com.gcd.vacancy.entity.CandidacyEntity;
+import org.mapstruct.Mapper;
+
+
+@Mapper(componentModel = "spring")
+public interface CandidacyMapper {
+
+//    ApplicationSentDto toApplicationSentDto(CandidacyEntity candidacyEntity);
+}
