3aac9b21d6d78cdc1e7aaff4a406c6bb78a900e0
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 23:31:11 -0300

    feat: VacancyPostDto created

---
+++ src/main/java/com/gcd/vacancy/dto/VacancyPostDto.java
@@ -0,0 +1,17 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+
+@Data
+public class VacancyPostDto {
+
+    private Long id;
+    private String title;
+    private String name_enterprise;
+    private String level;
+    private String remuneration;
+    private String modality;
+    private String description;
+    private Long enterpriseId;
+}
