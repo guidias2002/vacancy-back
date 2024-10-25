d3d42c4d25c933a93f284f2bb439817135cc4b92
Guilherme Dias <guidias200220@gmail.com>
2024-10-24 23:30:52 -0300

    feat: VacancyEntity created

--- src/main/java/com/gcd/vacancy/entity/VacancyEntity.java
+++ src/main/java/com/gcd/vacancy/entity/VacancyEntity.java
@@ -1,4 +1,47 @@
 package com.gcd.vacancy.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "vacancy")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class VacancyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String name_enterprise;
+
+    @Column(nullable = false)
+    private String level;
+
+    @Column(nullable = false)
+    private String remuneration;
+
+    @Column(nullable = false)
+    private String modality;
+
+    @Column(nullable = false)
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "enterprise_id", nullable = false)
+    private EnterpriseEntity enterprise;
+
+
 }
