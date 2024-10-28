3b294a665545dc451b6f65d0a8e08fa476433681
Guilherme Dias <guidias200220@gmail.com>
2024-10-28 12:02:18 -0300

    feat: CandidateWithApplicationsDto created

---
+++ src/main/java/com/gcd/vacancy/dto/CandidateWithApplicationsDto.java
@@ -0,0 +1,21 @@
+package com.gcd.vacancy.dto;
+
+
+import com.gcd.vacancy.entity.CandidacyEntity;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class CandidateWithApplicationsDto {
+
+    private Long id;
+    private String name;
+    private String login;
+    private String password;
+    private String email;
+    private String accountType;
+    private LocalDateTime createdAt;
+    private List<CandidacyEntity> candidacyEntityList;
+}
