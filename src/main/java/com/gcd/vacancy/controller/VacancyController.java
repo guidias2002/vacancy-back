318bdc50d3780ddcc49e67ba54956646a916ccb8
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 11:59:57 -0300

    feat: endpoint getlAllVacancy created

--- src/main/java/com/gcd/vacancy/controller/VacancyController.java
+++ src/main/java/com/gcd/vacancy/controller/VacancyController.java
@@ -1,11 +1,14 @@
 package com.gcd.vacancy.controller;
 
+import com.gcd.vacancy.dto.VacancyDto;
 import com.gcd.vacancy.dto.VacancyPostDto;
 import com.gcd.vacancy.service.VacancyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -22,5 +25,10 @@ public class VacancyController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @GetMapping("/getAll")
+    public ResponseEntity<List<VacancyDto>> getAllVacancy() {
+        List<VacancyDto> vacancyDtoList = vacancyService.getAllVacancy();
 
+        return ResponseEntity.ok(vacancyDtoList);
+    }
 }
