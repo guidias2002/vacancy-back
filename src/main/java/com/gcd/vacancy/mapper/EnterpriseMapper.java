3f58ae2529205f7f17d209bfbb5f96269f6b411b
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 12:01:38 -0300

    feat: converter for toEnterpriseDtoList created

--- src/main/java/com/gcd/vacancy/mapper/EnterpriseMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/EnterpriseMapper.java
@@ -6,6 +6,8 @@ import com.gcd.vacancy.dto.EnterpriseWithListVacanciesDto;
 import com.gcd.vacancy.entity.EnterpriseEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface EnterpriseMapper {
 
@@ -14,4 +16,6 @@ public interface EnterpriseMapper {
     EnterpriseDto toEnterpriseDto(EnterpriseEntity enterpriseEntity);
 
     EnterpriseWithListVacanciesDto toEnterpriseWithListVacanciesDto(EnterpriseEntity enterpriseEntity);
+
+    List<EnterpriseDto> toEnterpriseDtoList(List<EnterpriseEntity> enterpriseEntityList);
 }
