d9b0a698ffdee21c6dec12e5692c74b25c45c449
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 12:00:52 -0300

    feat: validations added to attributes

--- src/main/java/com/gcd/vacancy/dto/EnterprisePostDto.java
+++ src/main/java/com/gcd/vacancy/dto/EnterprisePostDto.java
@@ -1,16 +1,36 @@
 package com.gcd.vacancy.dto;
 
 import com.gcd.vacancy.enums.AccountType;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class EnterprisePostDto {
 
     private Long id;
+    @NotBlank(message = "O nome é obrigatório")
     private String name;
+
+    @NotBlank(message = "O CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter 14 dígitos numéricos")
     private String cnpj;
+
+    @NotBlank(message = "O login é obrigatório")
+    @Size(min = 6, message = "O login deve ter no mínimo 6 caracteres")
     private String login;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve estar em um formato válido")
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "A senha deve ter pelo menos uma letra maiúscula e um número")
     private String password;
     private final AccountType accountType = AccountType.ENTERPRISE;
 }
--- src/main/java/com/gcd/vacancy/entity/EnterpriseEntity.java
+++ src/main/java/com/gcd/vacancy/entity/EnterpriseEntity.java
@@ -2,6 +2,10 @@ package com.gcd.vacancy.entity;
 
 import com.gcd.vacancy.enums.AccountType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,18 +25,29 @@ public class EnterpriseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome é obrigatório")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "O CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{14}", message = "O CNPJ deve conter 14 dígitos numéricos")
     @Column(nullable = false, unique = true)
     private String cnpj;
 
+    @NotBlank(message = "O login é obrigatório")
+    @Size(min = 6, message = "O login deve ter no mínimo 6 caracteres")
     @Column(nullable = false, unique = true)
     private String login;
 
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "O email deve estar em um formato válido")
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "A senha deve ter pelo menos uma letra maiúscula e um número")
     @Column(nullable = false)
     private String password;
 
