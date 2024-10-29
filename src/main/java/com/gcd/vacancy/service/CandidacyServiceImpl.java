5d64a638baca805e2019e433ccaf728a4f57cd8f
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 20:10:15 -0300

    feat: refactored applyToVacancy method

--- src/main/java/com/gcd/vacancy/service/CandidacyServiceImpl.java
+++ src/main/java/com/gcd/vacancy/service/CandidacyServiceImpl.java
@@ -5,6 +5,7 @@ import com.gcd.vacancy.entity.CandidacyEntity;
 import com.gcd.vacancy.entity.CandidateEntity;
 import com.gcd.vacancy.entity.VacancyEntity;
 import com.gcd.vacancy.exceptions.customExceptions.NotFoundException;
+import com.gcd.vacancy.exceptions.customExceptions.ResourceAlreadyExistsException;
 import com.gcd.vacancy.mapper.CandidacyMapper;
 import com.gcd.vacancy.repository.CandidacyRepository;
 import com.gcd.vacancy.repository.CandidateRepository;
@@ -30,16 +31,13 @@ public class CandidacyServiceImpl implements CandidacyService {
     @Override
     public ApplicationSentDto applyToVacancy(Long vacancyId, Long candidateId) {
 
-        VacancyEntity vacancy = vacancyRepository.findById(vacancyId)
-                .orElseThrow(() -> new NotFoundException("Vaga com id " + vacancyId + " não encontrada."));
-
-        CandidateEntity candidate = candidateRepository.findById(candidateId)
-                .orElseThrow(() -> new NotFoundException("Candidato com id " + candidateId + " não encontrado."));
+        VacancyEntity vacancy = findVacancyById(vacancyId);
+        CandidateEntity candidate = findCandidateById(candidateId);
 
         boolean alreadyApplied = candidacyRepository.existsByCandidateIdAndVacancyId(candidateId, vacancyId);
 
         if (alreadyApplied) {
-            throw new IllegalArgumentException("O candidato já se inscreveu nessa vaga.");
+            throw new ResourceAlreadyExistsException("O candidato já se inscreveu na vaga " + "'" + vacancy.getTitle() + "'.");
         }
 
         CandidacyEntity newCandidacy = new CandidacyEntity();
@@ -55,6 +53,16 @@ public class CandidacyServiceImpl implements CandidacyService {
         ApplicationSentDto candidacyDto = candidacyMapper.toApplicationSentDto(newCandidacy);
 
         return candidacyDto;
+    }
+
+    private VacancyEntity findVacancyById(Long vacancyId) {
+        return vacancyRepository.findById(vacancyId)
+                .orElseThrow(() -> new NotFoundException("Vaga com id " + vacancyId + " não encontrada."));
+    }
+
+    private CandidateEntity findCandidateById(Long candidateId) {
+        return candidateRepository.findById(candidateId)
+                .orElseThrow(() -> new NotFoundException("Candidato com id " + candidateId + " não encontrada."));
     }
 
 
