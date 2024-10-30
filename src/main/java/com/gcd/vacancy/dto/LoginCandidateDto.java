def047e68e3253501cc7f91f1ac4af366c1a9670
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 23:53:01 -0300

    feat: validations added

--- src/main/java/com/gcd/vacancy/dto/LoginCandidateDto.java
+++ src/main/java/com/gcd/vacancy/dto/LoginCandidateDto.java
@@ -1,11 +1,15 @@
 package com.gcd.vacancy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginCandidateDto {
 
+    @NotBlank(message = "O campo loginOrEmail é obrigatório.")
     private String loginOrEmail;
+
+    @NotBlank(message = "O campo senha é obrigatório.")
     private String password;
 
 }
