d40f51d60027bce55ed8e277439f8214c81e2e0b
Guilherme Dias <guidias200220@gmail.com>
2024-10-30 12:10:27 -0300

    feat: toCandidacyListDto created

--- src/main/java/com/gcd/vacancy/mapper/CandidacyMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/CandidacyMapper.java
@@ -1,14 +1,19 @@
 package com.gcd.vacancy.mapper;
 
 import com.gcd.vacancy.dto.ApplicationSentDto;
+import com.gcd.vacancy.dto.CandidacyDto;
 import com.gcd.vacancy.entity.CandidacyEntity;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 
 @Mapper(componentModel = "spring")
 public interface CandidacyMapper {
 
     ApplicationSentDto toApplicationSentDto(CandidacyEntity candidacyEntity);
+
+    List<CandidacyDto> toCandidacyListDto(List<CandidacyEntity> candidacyEntityList);
 
 
 }
