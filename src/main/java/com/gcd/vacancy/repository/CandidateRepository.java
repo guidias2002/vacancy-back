286e2b7b5c09870676cb1fe734a2729c9a5af91d
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 21:32:49 -0300

    feat: method findByLoginOrEmail created

--- src/main/java/com/gcd/vacancy/repository/CandidateRepository.java
+++ src/main/java/com/gcd/vacancy/repository/CandidateRepository.java
@@ -2,11 +2,17 @@ package com.gcd.vacancy.repository;
 
 import com.gcd.vacancy.entity.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
 
     boolean existsByEmail(String email);
     boolean existsByLogin(String login);
+
+    @Query("SELECT c FROM CandidateEntity c WHERE c.login = :identifier OR c.email = :identifier")
+    Optional<CandidateEntity> findByLoginOrEmail(String identifier);
 }
