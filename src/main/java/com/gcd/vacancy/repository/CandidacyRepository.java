8958af0ef98178a80a92b649fd74403cdeb975b9
Guilherme Dias <guidias200220@gmail.com>
2024-10-30 12:10:45 -0300

    feat: findByEnterpriseId and findByCandidateId created

--- src/main/java/com/gcd/vacancy/repository/CandidacyRepository.java
+++ src/main/java/com/gcd/vacancy/repository/CandidacyRepository.java
@@ -4,8 +4,14 @@ import com.gcd.vacancy.entity.CandidacyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CandidacyRepository extends JpaRepository<CandidacyEntity, Long> {
 
     boolean existsByCandidateIdAndVacancyId(Long candidateId, Long vacancyId);
+
+    List<CandidacyEntity> findByEnterpriseId(Long enterpriseId);
+
+    List<CandidacyEntity> findByCandidateId(Long candidateId);
 }
