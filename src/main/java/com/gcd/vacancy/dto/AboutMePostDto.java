3407097ed210bf6933fdcf16cf7d5962d3a04ed6
Guilherme Dias <guidias200220@gmail.com>
2024-10-30 20:59:02 -0300

    feat: AboutMePostDto created

---
+++ src/main/java/com/gcd/vacancy/dto/AboutMePostDto.java
@@ -0,0 +1,13 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+@Data
+public class AboutMePostDto {
+
+    private String fullName;
+    private String location;
+    private String cellphoneNumber;
+    private String linkedin;
+    private Long candidateId;
+}
