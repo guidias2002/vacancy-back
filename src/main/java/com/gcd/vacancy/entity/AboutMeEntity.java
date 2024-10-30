31b8b8c58dca046fd7e20b0f971102d9d4964edf
Guilherme Dias <guidias200220@gmail.com>
2024-10-30 20:58:56 -0300

    feat: AboutMeEntity created

---
+++ src/main/java/com/gcd/vacancy/entity/AboutMeEntity.java
@@ -0,0 +1,25 @@
+package com.gcd.vacancy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "about_me")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AboutMeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String location;
+    private String cellphoneNumber;
+    private String linkedin;
+
+    @JoinColumn(name = "candidate_id")
+    private Long candidateId;
+}
