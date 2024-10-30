40ed969c39cb2ebc85e80bfc37c46284db38f802
Guilherme Dias <guidias200220@gmail.com>
2024-10-29 21:34:37 -0300

    feat: exclude = {SecurityAutoConfiguration.class} added

--- src/main/java/com/gcd/vacancy/VacancyApplication.java
+++ src/main/java/com/gcd/vacancy/VacancyApplication.java
@@ -2,8 +2,9 @@ package com.gcd.vacancy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class VacancyApplication {
 
 	public static void main(String[] args) {
