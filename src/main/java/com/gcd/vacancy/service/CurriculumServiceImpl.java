5d2cd79b61e937eb44b61c0ceee9d13c1776694d
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 12:22:56 -0300

    feat: associateAcademicExperienceWithCurriculum method created

---
+++ src/main/java/com/gcd/vacancy/service/CurriculumServiceImpl.java
@@ -0,0 +1,56 @@
+package com.gcd.vacancy.service;
+
+import com.gcd.vacancy.entity.AboutMeEntity;
+import com.gcd.vacancy.entity.AcademicExperienceEntity;
+import com.gcd.vacancy.entity.CandidateEntity;
+import com.gcd.vacancy.entity.CurriculumEntity;
+import com.gcd.vacancy.repository.CandidateRepository;
+import com.gcd.vacancy.repository.CurriculumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CurriculumServiceImpl {
+
+    @Autowired
+    private CurriculumRepository curriculumRepository;
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+
+    public void associateAboutMeWithCurriculum(AboutMeEntity aboutMeEntity, CandidateEntity candidate) {
+        CurriculumEntity curriculum = candidate.getCurriculum();
+
+        if(curriculum == null) {
+            curriculum = new CurriculumEntity();
+            curriculumRepository.save(curriculum);
+        }
+
+        curriculum.setAboutMe(aboutMeEntity);
+        curriculumRepository.save(curriculum);
+
+        candidate.setCurriculum(curriculum);
+        candidateRepository.save(candidate);
+    }
+
+    public void associateAcademicExperienceWithCurriculum(AcademicExperienceEntity academicExperienceEntity, CandidateEntity candidate) {
+        CurriculumEntity curriculum = candidate.getCurriculum();
+
+        if(curriculum == null) {
+            curriculum = new CurriculumEntity();
+            curriculumRepository.save(curriculum);
+        }
+
+        academicExperienceEntity.setCurriculumId(curriculum.getId());
+        academicExperienceEntity.setCandidateId(candidate.getId());
+
+        curriculum.getAcademicExperienceList().add(academicExperienceEntity);
+        curriculumRepository.save(curriculum);
+
+        candidate.setCurriculum(curriculum);
+        candidateRepository.save(candidate);
+    }
+}
