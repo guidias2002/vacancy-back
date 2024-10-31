8420f536eb94477fd5b1c880643b360e2aab2c54
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 20:19:31 -0300

    feat: validation fields added

--- src/main/java/com/gcd/vacancy/dto/AboutMePostDto.java
+++ src/main/java/com/gcd/vacancy/dto/AboutMePostDto.java
@@ -1,12 +1,21 @@
 package com.gcd.vacancy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AboutMePostDto {
 
+
+    @NotBlank(message = "Campo obrigatório")
     private String fullName;
+
+    @NotBlank(message = "Campo obrigatório")
     private String location;
+
+    @NotBlank(message = "Campo obrigatório")
     private String cellphoneNumber;
+
+    @NotBlank(message = "Campo obrigatório")
     private String linkedin;
 }
--- src/main/java/com/gcd/vacancy/entity/AboutMeEntity.java
+++ src/main/java/com/gcd/vacancy/entity/AboutMeEntity.java
@@ -1,6 +1,7 @@
 package com.gcd.vacancy.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +16,19 @@ public class AboutMeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Campo obrigatório")
     private String fullName;
+
+    @NotBlank(message = "Campo obrigatório")
     private String location;
+
+    @NotBlank(message = "Campo obrigatório")
     private String cellphoneNumber;
+
+    @NotBlank(message = "Campo obrigatório")
     private String linkedin;
+
     private Long candidateId;
+    private Long curriculumId;
 }
