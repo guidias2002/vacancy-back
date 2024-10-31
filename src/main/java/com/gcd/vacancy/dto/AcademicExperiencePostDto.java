ccb5fc518d7af434621f95900582137d2c0c6250
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 12:21:45 -0300

    feat: AcademicExperiencePostDto created

---
+++ src/main/java/com/gcd/vacancy/dto/AcademicExperiencePostDto.java
@@ -0,0 +1,12 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+@Data
+public class AcademicExperiencePostDto {
+
+    private String course;
+    private String institution;
+    private String level;
+    private int completion;
+}
