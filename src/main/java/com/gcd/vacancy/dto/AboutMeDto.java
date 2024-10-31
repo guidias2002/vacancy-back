0fce301402dd4bf8220fa4ed2e32476ff47d4fa0
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 20:19:09 -0300

    feat: AboutMeDto created

---
+++ src/main/java/com/gcd/vacancy/dto/AboutMeDto.java
@@ -0,0 +1,12 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+@Data
+public class AboutMeDto {
+
+    private String fullName;
+    private String location;
+    private String cellphoneNumber;
+    private String linkedin;
+}
