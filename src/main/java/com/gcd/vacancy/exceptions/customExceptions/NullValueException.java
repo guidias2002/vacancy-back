2ea1e88edc23ac4f46d697bd44a01ac0afa36245
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 20:22:44 -0300

    feat: NullValueException created

---
+++ src/main/java/com/gcd/vacancy/exceptions/customExceptions/NullValueException.java
@@ -0,0 +1,8 @@
+package com.gcd.vacancy.exceptions.customExceptions;
+
+public class NullValueException extends RuntimeException {
+
+    public NullValueException(String message) {
+        super(message);
+    }
+}
