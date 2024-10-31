4a80ee50aecc27ce480178fe76c36d404d29413b
Guilherme Dias <guidias200220@gmail.com>
2024-10-31 20:20:02 -0300

    feat: findAboutMeByCandidateId method created

--- src/main/java/com/gcd/vacancy/repository/AboutMeRepository.java
+++ src/main/java/com/gcd/vacancy/repository/AboutMeRepository.java
@@ -4,6 +4,10 @@ import com.gcd.vacancy.entity.AboutMeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AboutMeRepository extends JpaRepository<AboutMeEntity, Long> {
+
+    Optional<AboutMeEntity> findAboutMeByCandidateId(Long candidateId);
 }
