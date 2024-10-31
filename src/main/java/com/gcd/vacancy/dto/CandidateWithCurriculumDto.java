78eebe69f8866c81ef2ebc25cab109a6f1c508de
Guilherme Dias <guidias200220@gmail.com>
2024-10-30 21:02:21 -0300

    feat: CandidateWithCurriculumDto created

---
+++ src/main/java/com/gcd/vacancy/dto/CandidateWithCurriculumDto.java
@@ -0,0 +1,19 @@
+package com.gcd.vacancy.dto;
+
+import com.gcd.vacancy.entity.CurriculumEntity;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CandidateWithCurriculumDto {
+
+    private Long id;
+    private String name;
+    private String login;
+    private String password;
+    private String email;
+    private String accountType;
+    private LocalDateTime createdAt;
+    private CurriculumEntity curriculum;
+}
