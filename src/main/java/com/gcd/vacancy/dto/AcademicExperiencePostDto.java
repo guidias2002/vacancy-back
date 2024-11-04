d8b62c3ebf943ddf49af3dde1aaae42b83e5ceb7
Guilherme Dias <guidias200220@gmail.com>
2024-11-04 20:46:00 -0300

    feat: validation for the updated attribute

--- src/main/java/com/gcd/vacancy/dto/AcademicExperiencePostDto.java
+++ src/main/java/com/gcd/vacancy/dto/AcademicExperiencePostDto.java
@@ -1,6 +1,7 @@
 package com.gcd.vacancy.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -15,6 +16,6 @@ public class AcademicExperiencePostDto {
     @NotBlank(message = "Campo obrigatório.")
     private String level;
 
-    @NotBlank(message = "Campo obrigatório.")
+    @NotNull(message = "Campo obrigatório.")
     private Long completion;
 }
