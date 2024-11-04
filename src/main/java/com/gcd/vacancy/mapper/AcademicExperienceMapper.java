144b2be6a8ff18a25db07f5ff0f8fc825b4d3067
Guilherme Dias <guidias200220@gmail.com>
2024-11-04 17:11:07 -0300

    feat: toAcademicExperienceDto method created

--- src/main/java/com/gcd/vacancy/mapper/AcademicExperienceMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/AcademicExperienceMapper.java
@@ -1,5 +1,6 @@
 package com.gcd.vacancy.mapper;
 
+import com.gcd.vacancy.dto.AcademicExperienceDto;
 import com.gcd.vacancy.dto.AcademicExperiencePostDto;
 import com.gcd.vacancy.entity.AcademicExperienceEntity;
 import org.mapstruct.Mapper;
@@ -8,4 +9,6 @@ import org.mapstruct.Mapper;
 public interface AcademicExperienceMapper {
 
     AcademicExperienceEntity toAcaAcademicExperienceEntity(AcademicExperiencePostDto academicExperiencePostDto);
+
+    AcademicExperienceDto toAcademicExperienceDto(AcademicExperienceEntity academicExperienceEntity);
 }
