e8cedc4f93162615efe3b804cecd6b35cc83b10f
Guilherme Dias <guidias200220@gmail.com>
2024-11-04 17:11:19 -0300

    feat: AcademicExperiencePostDto created

--- src/main/java/com/gcd/vacancy/dto/AcademicExperiencePostDto.java
+++ src/main/java/com/gcd/vacancy/dto/AcademicExperiencePostDto.java
@@ -1,12 +1,20 @@
 package com.gcd.vacancy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AcademicExperiencePostDto {
 
+    @NotBlank(message = "Campo obrigatório.")
     private String course;
+
+    @NotBlank(message = "Campo obrigatório.")
     private String institution;
+
+    @NotBlank(message = "Campo obrigatório.")
     private String level;
+
+    @NotBlank(message = "Campo obrigatório.")
     private Long completion;
 }
