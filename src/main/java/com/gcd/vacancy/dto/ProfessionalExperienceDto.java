a5e02f6c5a6d7f50bf536c3d5529011e3e7b6928
Guilherme Dias <guidias200220@gmail.com>
2024-11-04 17:13:59 -0300

    feat: ProfessionalExperienceDto created

---
+++ src/main/java/com/gcd/vacancy/dto/ProfessionalExperienceDto.java
@@ -0,0 +1,12 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+@Data
+public class ProfessionalExperienceDto {
+
+    private String enterprise;
+    private String position;
+    private String period;
+    private String description;
+}
