3ca61ab206bb7c27aa4ccf57c8d26a567a40c7bf
Guilherme Dias <guidias200220@gmail.com>
2024-11-04 20:43:25 -0300

    feat: validation params added

--- src/main/java/com/gcd/vacancy/dto/LanguagePostDto.java
+++ src/main/java/com/gcd/vacancy/dto/LanguagePostDto.java
@@ -1,10 +1,14 @@
 package com.gcd.vacancy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LanguagePostDto {
 
+    @NotBlank(message = "O campo é obrigatório.")
     private String language;
+
+    @NotBlank(message = "O campo é obrigatório.")
     private String level;
 }
