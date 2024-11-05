c619d5fe5b696a39f663221c27d24181ab1a2318
Guilherme Dias <guidias200220@gmail.com>
2024-11-05 09:55:44 -0300

    feat: findByLanguageIgnoreCase method created

--- src/main/java/com/gcd/vacancy/repository/LanguageRepository.java
+++ src/main/java/com/gcd/vacancy/repository/LanguageRepository.java
@@ -4,6 +4,10 @@ import com.gcd.vacancy.entity.LanguageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Optional;
+
 @ResponseStatus
 public interface LanguageRepository extends JpaRepository<LanguageEntity, Long> {
+
+    Optional<LanguageEntity> findByLanguageIgnoreCase(String language);
 }
