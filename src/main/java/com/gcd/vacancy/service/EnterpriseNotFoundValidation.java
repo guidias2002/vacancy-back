018af22dea8e348c3c5a9c936b855ea9e4718b18
Guilherme Dias <guidias200220@gmail.com>
2024-11-05 10:26:44 -0300

    feat: EnterpriseNotFoundValidation method created

---
+++ src/main/java/com/gcd/vacancy/service/EnterpriseNotFoundValidation.java
@@ -0,0 +1,17 @@
+package com.gcd.vacancy.service;
+
+import com.gcd.vacancy.entity.EnterpriseEntity;
+import com.gcd.vacancy.exceptions.customExceptions.NotFoundException;
+import com.gcd.vacancy.repository.EnterpriseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EnterpriseNotFoundValidation {
+
+    @Autowired
+    private EnterpriseRepository enterpriseRepository;
+
+    public EnterpriseEntity findEnterpriseById(Long enterpriseId) {
+        return enterpriseRepository.findById(enterpriseId)
+                .orElseThrow(() -> new NotFoundException("Empresa com id " + enterpriseId + " não encontrada."));
+    }
+}
