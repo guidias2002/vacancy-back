46f914a1e48a3f0d8cf80f3268b44db060595746
Guilherme Dias <guidias200220@gmail.com>
2024-11-11 15:50:25 -0300

    feat: method renamed

--- src/main/java/com/gcd/vacancy/mapper/AboutCompanyMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/AboutCompanyMapper.java
@@ -3,10 +3,10 @@ package com.gcd.vacancy.mapper;
 import com.gcd.vacancy.dto.AboutCompanyPostDto;
 import com.gcd.vacancy.entity.AboutCompanyEntity;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 public interface AboutCompanyMapper {
 
     AboutCompanyEntity toAboutCompanyEntity(AboutCompanyPostDto aboutCompanyPostDto);
+
 }
