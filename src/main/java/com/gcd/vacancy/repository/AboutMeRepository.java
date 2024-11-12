559be48162f6589681aff73b84f2f25813302f4e
Guilherme Dias <guidias200220@gmail.com>
2024-11-12 09:58:44 -0300

    feat: existsByCandidateId method created

--- src/main/java/com/gcd/vacancy/repository/AboutMeRepository.java
+++ src/main/java/com/gcd/vacancy/repository/AboutMeRepository.java
@@ -1,5 +1,6 @@
 package com.gcd.vacancy.repository;
 
+import com.gcd.vacancy.dto.AboutMeDto;
 import com.gcd.vacancy.entity.AboutMeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface AboutMeRepository extends JpaRepository<AboutMeEntity, Long> {
 
     Optional<AboutMeEntity> findAboutMeByCandidateId(Long candidateId);
+
+    Boolean existsByCandidateId(Long candidateId);
 }
