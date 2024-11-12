b5c2c4128ec0362c943a62a6ccc6e35fccd1c3c8
Guilherme Dias <guidias200220@gmail.com>
2024-11-12 20:54:07 -0300

    feat: findAllByCandidateId method created

--- src/main/java/com/gcd/vacancy/repository/ProfessionalExperienceRepository.java
+++ src/main/java/com/gcd/vacancy/repository/ProfessionalExperienceRepository.java
@@ -4,6 +4,10 @@ import com.gcd.vacancy.entity.ProfessionalExperienceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfessionalExperienceRepository extends JpaRepository<ProfessionalExperienceEntity, Long> {
+
+    List<ProfessionalExperienceEntity> findAllByCandidateId(Long candidateId);
 }
