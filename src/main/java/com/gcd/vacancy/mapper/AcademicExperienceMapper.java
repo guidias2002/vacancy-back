92bafe4ed576f8516eb1154da619465dd5c88a7f
Guilherme Dias <guidias200220@gmail.com>
2024-11-26 12:06:43 -0300

    feat: toAcademicExperienceDtoList method created

--- src/main/java/com/gcd/vacancy/mapper/AcademicExperienceMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/AcademicExperienceMapper.java
@@ -5,10 +5,13 @@ import com.gcd.vacancy.dto.AcademicExperiencePostDto;
 import com.gcd.vacancy.entity.AcademicExperienceEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface AcademicExperienceMapper {
 
     AcademicExperienceEntity toAcaAcademicExperienceEntity(AcademicExperiencePostDto academicExperiencePostDto);
 
     AcademicExperienceDto toAcademicExperienceDto(AcademicExperienceEntity academicExperienceEntity);
+    List<AcademicExperienceDto> toAcademicExperienceDtoList(List<AcademicExperienceEntity> academicExperienceEntity);
 }
