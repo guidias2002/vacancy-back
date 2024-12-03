ea1cd65199b3ffa86c5049e4fa373d7ee82d3c98
Guilherme Dias <guidias200220@gmail.com>
2024-12-03 11:22:15 -0300

    feat: findSkillsByCandidateId created

--- src/main/java/com/gcd/vacancy/repository/SkillRepository.java
+++ src/main/java/com/gcd/vacancy/repository/SkillRepository.java
@@ -4,10 +4,13 @@ import com.gcd.vacancy.entity.SkillEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SkillRepository extends JpaRepository<SkillEntity, Long> {
 
     Optional<SkillEntity> findBySkillIgnoreCase(String skill);
+
+    List<SkillEntity> findSkillsByCandidateId(Long candidateId);
 }
