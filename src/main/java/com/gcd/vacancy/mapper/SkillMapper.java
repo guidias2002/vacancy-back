c9644e3c277a92369aefa9ae52149781683e70d2
Guilherme Dias <guidias200220@gmail.com>
2024-12-03 11:22:05 -0300

    feat: toSkillDtoList created

--- src/main/java/com/gcd/vacancy/mapper/SkillMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/SkillMapper.java
@@ -1,12 +1,17 @@
 package com.gcd.vacancy.mapper;
 
+import com.gcd.vacancy.dto.SkillDto;
 import com.gcd.vacancy.dto.SkillPostDto;
 import com.gcd.vacancy.entity.SkillEntity;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SkillMapper {
 
     SkillEntity toSkillEntity(SkillPostDto skillPostDto);
+
+    List<SkillDto> toSkillDtoList(List<SkillEntity> skillEntityList);
 }
 
