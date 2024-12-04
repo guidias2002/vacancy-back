cfade3a1c2661549aed25d1ec05ea874242c0926
Guilherme Dias <guidias200220@gmail.com>
2024-12-04 12:24:29 -0300

    feat: toLanguageDtoList created

--- src/main/java/com/gcd/vacancy/mapper/LanguageMapper.java
+++ src/main/java/com/gcd/vacancy/mapper/LanguageMapper.java
@@ -5,10 +5,14 @@ import com.gcd.vacancy.dto.LanguagePostDto;
 import com.gcd.vacancy.entity.LanguageEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface LanguageMapper {
 
     LanguageEntity toLanguageEntity(LanguagePostDto languagePostDto);
 
     LanguageDto toLanguageDto(LanguageEntity languageEntity);
+
+    List<LanguageDto> toLanguageDtoList(List<LanguageEntity> languageEntityList);
 }
