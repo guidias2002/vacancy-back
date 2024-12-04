4312ff3cdec7aac838d676634f7452e89c9e8cb6
Guilherme Dias <guidias200220@gmail.com>
2024-12-04 12:24:23 -0300

    feat: findLanguagesByCandidateId created

--- src/main/java/com/gcd/vacancy/repository/LanguageRepository.java
+++ src/main/java/com/gcd/vacancy/repository/LanguageRepository.java
@@ -1,13 +1,17 @@
 package com.gcd.vacancy.repository;
 
+import com.gcd.vacancy.dto.LanguageDto;
 import com.gcd.vacancy.entity.LanguageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.List;
 import java.util.Optional;
 
 @ResponseStatus
 public interface LanguageRepository extends JpaRepository<LanguageEntity, Long> {
 
     Optional<LanguageEntity> findByLanguageIgnoreCase(String language);
+
+    List<LanguageEntity> findLanguagesByCandidateId(Long candidateId);
 }
