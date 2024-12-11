f01ed159d99603bcab4bf1bf8253dffe1c27fb30
Guilherme Dias <guidias200220@gmail.com>
2024-12-11 12:06:39 -0300

    feat: RecruiterEntity created

---
+++ src/main/java/com/gcd/vacancy/entity/RecruiterEntity.java
@@ -0,0 +1,44 @@
+package com.gcd.vacancy.entity;
+
+import com.gcd.vacancy.enums.AccountType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "recruiter")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecruiterEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank(message = "O email é obrigatório")
+    @Email(message = "Email inválido")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "enterprise_id", nullable = false)
+    private EnterpriseEntity enterprise;
+
+    @Enumerated(EnumType.STRING)
+    private final AccountType accountType = AccountType.RECRUITER;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
+}
