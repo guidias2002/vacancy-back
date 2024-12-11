957edeeb65adf43dfd73711b3fdd5c70dc50eb6f
Guilherme Dias <guidias200220@gmail.com>
2024-12-11 19:51:17 -0300

    feat: RecruiterMapper created

---
+++ src/main/java/com/gcd/vacancy/mapper/RecruiterMapper.java
@@ -0,0 +1,15 @@
+package com.gcd.vacancy.mapper;
+
+import com.gcd.vacancy.dto.RecruiterDto;
+import com.gcd.vacancy.entity.RecruiterEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RecruiterMapper {
+
+    RecruiterDto toRecruiterDto(RecruiterEntity recruiterEntity);
+
+    List<RecruiterDto> toRecruiterDtoList(List<RecruiterEntity> recruiterEntityList);
+}
