ee7d982083e0eb34a91173ebde8b37f52428b076
Guilherme Dias <guidias200220@gmail.com>
2024-12-11 19:51:29 -0300

    feat: findRecruiterByEmail created

--- src/main/java/com/gcd/vacancy/repository/RecruiterRepository.java
+++ src/main/java/com/gcd/vacancy/repository/RecruiterRepository.java
@@ -4,8 +4,10 @@ import com.gcd.vacancy.entity.RecruiterEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecruiterRepository extends JpaRepository<RecruiterEntity, Long> {
 
-
+    Optional<RecruiterEntity> findRecruiterByEmail(String email);
 }
