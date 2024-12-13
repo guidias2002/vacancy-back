bbd777d87186f92c0456365b2324094f2c259e4b
Guilherme Dias <guidias200220@gmail.com>
2024-12-13 16:12:46 -0300

    feat: added validations

--- src/main/java/com/gcd/vacancy/dto/UpdatedPasswordDto.java
+++ src/main/java/com/gcd/vacancy/dto/UpdatedPasswordDto.java
@@ -1,6 +1,8 @@
 package com.gcd.vacancy.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,14 @@ import lombok.Setter;
 public class UpdatedPasswordDto {
 
     @NotBlank(message = "Campo obrigatório.")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "A senha deve ter pelo menos uma letra maiúscula e um número")
     private String newPassword;
 
     @NotBlank(message = "Campo obrigatório.")
+    @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d).+$",
+            message = "A senha deve ter pelo menos uma letra maiúscula e um número")
     private String NewPasswordConfirmed;
 }
