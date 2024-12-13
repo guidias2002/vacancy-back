de44343c7692f9dc4c5f614b7551f09227a1d8bb
Guilherme Dias <guidias200220@gmail.com>
2024-12-13 16:04:49 -0300

    feat: @async added

--- src/main/java/com/gcd/vacancy/service/EmailServiceImpl.java
+++ src/main/java/com/gcd/vacancy/service/EmailServiceImpl.java
@@ -3,9 +3,9 @@ package com.gcd.vacancy.service;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +17,7 @@ public class EmailServiceImpl implements EmailService{
     @Value("${spring.mail.username}")
     private String sender;
 
+    @Async
     @Override
     public String sendInvitationToRecruiter(String recipientEmail, String password, String subject, String enterpriseName) {
 
