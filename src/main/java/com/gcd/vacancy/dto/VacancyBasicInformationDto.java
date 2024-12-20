0c18f29191f2cc3ac9d9df9959829f4e270002c8
Guilherme Dias <guidias200220@gmail.com>
2024-12-20 12:17:58 -0300

    feat: VacancyBasicInformationDto created

---
+++ src/main/java/com/gcd/vacancy/dto/VacancyBasicInformationDto.java
@@ -0,0 +1,20 @@
+package com.gcd.vacancy.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class VacancyBasicInformationDto {
+
+    private Long id;
+    private String title;
+    private String name_enterprise;
+    private String level;
+    private String remuneration;
+    private String modality;
+    private String description;
+    private String location;
+    private Long enterpriseId;
+    private LocalDateTime createdAt;
+}
