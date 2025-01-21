54b961c885e6abc6bd409d37bc844f7cc52fca56
Guilherme Dias <guidias200220@gmail.com>
2025-01-21 20:00:32 -0300

    feat: findByStatus method created

--- src/main/java/com/gcd/vacancy/repository/VacancyRepository.java
+++ src/main/java/com/gcd/vacancy/repository/VacancyRepository.java
@@ -1,6 +1,8 @@
 package com.gcd.vacancy.repository;
 
+import com.gcd.vacancy.dto.VacancyDto;
 import com.gcd.vacancy.entity.VacancyEntity;
+import com.gcd.vacancy.enums.VacancyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface VacancyRepository extends JpaRepository<VacancyEntity, Long> {
 
     List<VacancyEntity> findVacancyByEnterpriseId(Long enterpriseId);
+
+    List<VacancyEntity> findByStatus(VacancyStatus status);
 }
